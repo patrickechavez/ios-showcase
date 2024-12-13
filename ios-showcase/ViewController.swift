15bf92f15873b05424e557de1ab61688d7db22fa
johnpatrick.echavez <johnpatrickechavez-cp@coreproc.com>
2024-12-13 17:57:59 +0800

    Initial Commit

---
+++ ios-showcase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-showcase
+//
+//  Created by John Patrick Echavez on 12/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
